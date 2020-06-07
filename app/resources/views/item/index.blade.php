2943141b38b7cb257d36a11816e2fb2b027d93d3
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2020-06-07 18:25:16 +0900

    脱laradock_2

---
+++ app/resources/views/item/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    @if(Session::has('flash_message'))
+	<div class="alert alert-success">
+	    {{ session('flash_message') }}
+	</div>
+    @endif
+    <div class="container">
+        <div class="row justify-content-left">
+            @foreach ($items as $item)
+            <div class="col-md-4 mb-2">
+              <div class="card">
+		            <div class="card-header">
+			          <a href="/item/{{ $item->id }}">{{ $item->name }}</a>
+		            </div>
+                <div class="card-body">
+                  {{ $item->amount }}円
+		            </div>
+		             @auth
+			            <form method="POST" action="cartitem" class="form-inline m-1">
+			            {{ csrf_field() }}
+			            <select name="quantity" class="form-control col-md-2 mr-1">
+			              <option selected>1</optino>
+			              <option>2</optino>
+			              <option>3</optino>
+			              <option>4</optino>
+			              <option>5</optino>
+			            </select>
+			            <input type="hidden" name="item_id" value="{{ $item->id }}">
+			            <button type="submit" class="btn btn-primary col-md-6">カートに入れる</button>
+                  
+                  <!--お気にいいり機能-->
+                  <div class="card-body pt-0 pb-2 pl-3">
+                    <div class="card-text">
+                      <article-like>
+                      </article-like>
+                    </div>
+                  </div>
+                  
+                  
+                  </form>
+		             @endauth
+              </div>
+            </div>
+            @endforeach
+        </div>
+        <!--
+        <div>
+          <carousel>
+          </carousel>
+        </div>
+        -->
+        <div class="row justify-content-center">
+            {{ $items->appends(['keyword' => Request::get('keyword')])->links() }}
+        </div>
+    </div>
+@endsection
